a93dd264b24579085b47838bd34b00002686167a
kirti maurya <kirtimaurya@kirtis-MacBook-Air.local>
2024-04-11 16:18:03 +0530

    updates

--- App.js
+++ App.js
@@ -1,5 +1,46 @@
-const parent = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"}, React.createElement("h1", {id: "child_heading"}, "I'm an h1 tag")))
-const heading = React.createElement("h1", {id: "heading"}, "Hello World from react");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// React Element - these are object - when render it becomes html element 
+
+const heading = React.createElement
+("h1",
+ {}, 
+ "Namaste React"
+ );
+ 
+
+ // JSX - it is a syntax which is easier to create ReactElement - not a part of React - merge js and html - it is a html-like or xml-like syntax
+// it is transpiled (by Parcel and it dependecy Babel) before it reaches to JS engine
+
+const jsxHeading = (
+   <h1 id="heading" className="heading">
+      Namaste React using JSX
+    </h1>
+    );
+
+// React Components: 
+  // Class based components: old way of writing code
+  
+  // Functional components: New way - function that returns a JSX code - always starts with caps
+const Title = () => (
+     <h1 className="heading">Namaste React Using JSX</h1>
+);
+
+const title =  (
+    <h1 className="heading">Namaste React Using JSX</h1>
+);
+
+const number = 1000;
+// Component Composition
+  const HeadingComponent = () => (
+    <div id="container">
+        <Title />
+        {title}
+     <h1 className="heading">Namaste React Functional Component</h1>
+     </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent/>);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Namaste React 
+
+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
+
+# Parcel - parceljs.org
+- Dev Build
+- Local Server
+- HMR (Hot Module Replacement)
+- File Watching algorithm - written in c++
+- Caching - Faster Builds
+- Image Optimisation
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code splitting
+- Differential Bundling - when app is hosted, app can be be opened up in any browser , so it supports older browsers
+- Diagnostics of app
+- Better Error Handling
+- HTTPs
+- Tree Shaking - remove unused code 
+- Different dev and prod bundles
